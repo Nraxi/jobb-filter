c5450ac2070d4b0e833333d3d95be1b4d839fe09
nraxi <olle.code@hotmail.com>
2026-04-09 20:15:09 +0200

    add deployment pnpm

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/af-api': {
-        target: 'https://ads.api.jobtechdev.se', // Se till att det inte finns "/" i slutet här
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/af-api/, ''),
-      }
-    }
-  }
+   base: '/jobb-filter/'
 })
